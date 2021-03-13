f0ae079818a47a8982187c75d47f37199ac5eef5
DianaDy01 <dianamariapetre006@gmail.com>
2021-03-13 10:33:34 +0200

    idk

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -1,80 +1,100 @@
 // @ts-check
+
 class Hero extends Phaser.GameObjects.Sprite {
 
-   keyLeft;
-   keyRight;
+    keyLeft;
+    keyRight;
+    keyJump;
 
-   heroState = 'idle';
-   animState = 'idle';
+    heroState = 'idle';
+    animState = 'idle';
 
+    constructor(scene, x, y) {
+        super(scene, x, y, 'mage');
 
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
 
-   constructor(scene, x, y) {
-      super(scene, x, y, 'mage');
-      scene.add.existing(this);
-      scene.physics.add.existing(this);
+        if (!(this.body instanceof Phaser.Physics.Arcade.Body)) {
+            return;
+        }
 
-      if (!(this.body instanceof Phaser.Physics.Arcade.Body)) {
-         return;
-      }
+        this.body.setCollideWorldBounds(true);
+        this.body.setSize(33, 54);
+        this.body.setOffset(70, 57);
+        this.anims.play('hero-idle');
+        this.body.setDragX(500);
 
+        this.keyLeft = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.keyRight = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.keyJump = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
 
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
 
-      this.body.setCollideWorldBounds(true);
-      this.body.setSize(33, 54);
-      this.body.setOffset(70, 57);
-      this.anims.play('hero-idle');
-      this.body.setDragX(1000)
-
-      this.keyLeft = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-      this.keyRight = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-
-
-   }
-   preUpdate(time, delta) {
-      super.preUpdate(time, delta);
-
-      if (!(this.body instanceof Phaser.Physics.Arcade.Body)) {
-         return;
-      }
-
-      if (this.keyLeft.isUp && this.keyRight.isUp) {
-         this.body.setAccelerationX(0);
-         this.heroState = "idle";
-      }
-
-      if (this.keyLeft.isDown) {
-         //this.body.setVelocityX(-500);
-         this.body.setMaxVelocity(200, 400);
-         this.body.setAcceleration(-500)
-         this.anims.play('hero-walk');
-         this.setFlipX(true);
-         this.heroState = "walk";
-      }
-
-      if (this.keyRight.isDown) {
-         // this.body.setVelocityX(500);
-         this.body.setMaxVelocity(200, 400);
-         this.body.setAcceleration(500)
-         this.anims.play('hero-walk');
-         this.setFlipX(false);
-         this.heroState = "walk";
+        if (!(this.body instanceof Phaser.Physics.Arcade.Body)) {
+            return;
+        }
 
 
-      }
+        if (this.keyLeft.isUp && this.keyRight.isUp && this.body.onFloor() && this.body.velocity.y == 0) {
+            this.body.setAccelerationX(0);
+            this.heroState = "idle";
+        }
 
-      if (this.heroState == "idle" && this.animState != "idle") {
-         this.anims.play('hero-idle');
-         this.animState = "idle";
-      }
-      if (this.heroState == "walk" && this.animState != "walk") {
-         this.anims.play('hero-walk');
-         this.animState = "walk";
-         console.log(this.heroState + " , " + this.animState);
+        if (this.keyLeft.isDown && this.body.onFloor() && this.body.velocity.y == 0) {
+            //this.body.setVelocityX(-500);
+            this.body.setMaxVelocity(200, 400);
+            this.body.setAccelerationX(-500);
+            this.setFlipX(true);
+            this.heroState = "walk";
+        }
+        if (this.keyRight.isDown && this.body.onFloor() && this.body.velocity.y == 0) {
+            //this.body.setVelocityX(500);
+            this.body.setMaxVelocity(200, 400);
+            this.body.setAccelerationX(500);
+            this.setFlipX(false);
+            this.heroState = "walk";
+        }
+              let justDown = Phaser.Input.Keyboard.JustDown(this.keyJump);
 
-      }
-      
-   }
+        if (justDown && this.heroState != 'jump' && this.body.onFloor() && this.body.velocity.y == 0) {
+            this.body.setVelocityY(-550);
+            justDown = false
+            this.heroState = 'jump';
+        }
+
+        if (justDown && this.heroState == 'jump') {
+            this.body.setVelocityY(-400)
+            this.heroState = 'double-jump'
+
+
+
+        }
+
+        if (this.heroState == "idle" && this.animState != "idle") {
+            this.anims.play('hero-idle');
+            this.animState = "idle";
+        }
+        if (this.heroState == "walk" && this.animState != "walk") {
+            this.anims.play('hero-walk');
+            this.animState = "walk";
+        }
+        if (this.heroState == 'jump' && this.animState != 'jump') {
+            this.anims.play('hero-jump');
+            this.animState = 'jump';
+        }
+
+        if (this.heroState == 'double-jump' && this.animState != 'double-jump') {
+            this.anims.play('hero-double-jump');
+            this.animState = 'double-jump';
+        }
+
+        console.log("heroState:" + this.heroState + " animState:" + this.animState);
+
+    }
+
 }
 
 export default Hero;
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,38 +3,59 @@
 
 import Phaser from 'phaser';
 import Hero from '../entities/Hero.js';
+
 class Game extends Phaser.Scene {
-    preload() {
-        this.load.image('mage', 'assets/mage/mage.png');
-        this.load.spritesheet('idle-spritesheet', 'assets/mage/idle.png', { frameWidth: 171, frameHeight: 128 });
-        this.load.spritesheet('walk-spritesheet', 'assets/mage/walk.png', { frameWidth: 171, frameHeight: 128 });
-      }
-      
-      create() {
-        this.anims.create({
-          key: 'hero-idle',
-          frames: [
-            { frame: 0, key: 'mage', duration: 5000 },
-            ...this.anims.generateFrameNumbers('idle-spritesheet', {})
-          ],
-          frameRate: 6,
-          repeat: -1
-        });
 
-        this.anims.create({
-            key: 'hero-walk',
-            frames:this.anims.generateFrameNumbers('walk-spritesheet',{}),
-            frameRate: 6,
-            repeat: -1
-        });
+  preload() {
+    this.load.image('mage', 'assets/mage/mage.png');
+    this.load.spritesheet('idle-spritesheet', 'assets/mage/idle.png', { frameWidth: 171, frameHeight: 128 });
+    this.load.spritesheet('walk-spritesheet', 'assets/mage/walk.png', { frameWidth: 171, frameHeight: 128 });
+    this.load.spritesheet('jump-spritesheet', 'assets/mage/jump.png', { frameWidth: 171, frameHeight: 128 });
+    this.load.spritesheet('double-jump-spritesheet', 'assets/mage/double-jump.png', { frameWidth: 171, frameHeight: 128 });
+  }
+
+  create() {
+    this.anims.create({
+      key: 'hero-idle',
+      frames: [
+        { frame: 0, key: 'mage', duration: 5000 },
+        ...this.anims.generateFrameNumbers('idle-spritesheet', {})
+      ],
+      frameRate: 6,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'hero-walk',
+      frames: this.anims.generateFrameNumbers('walk-spritesheet', {}),
+      frameRate: 6,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'hero-jump',
+      frames: this.anims.generateFrameNumbers('jump-spritesheet', {}),
+      frameRate: 6,
+      repeat: -1
     
-        let hero = new Hero(this,400, 300);
-    
-        
-      }
+    });
+
+    this.anims.create({
+      key: 'hero-double-jump',
+      frames: this.anims.generateFrameNumbers('double-jump-spritesheet', {}),
+      frameRate: 15,
+      repeat: 0
+    });
 
 
-    }
-  
- export default Game
+    let hero = new Hero(this, 400, 300);
+
+
+
+  }
+
+
+}
+
+export default Game;
  
